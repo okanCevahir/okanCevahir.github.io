155299bdf61788e77dcf1a2070a4952847407d07
Okan Cevahir <cevahir.okan1@gmail.com>
2023-04-22 23:51:54 +0300

    Add Index File!

---
+++ wwwroot/js/splash.js
@@ -0,0 +1,18 @@
+﻿export class HomePage {
+    tabBarElement: any;
+    splash = true;
+
+    constructor(public navCtrl: NavController) {
+        this.tabBarElement = document.querySelector('.tabbar');
+    }
+
+    ionViewDidLoad() {
+        this.tabBarElement.style.display = 'none';
+        setTimeout(() => {
+            this.splash = false;
+            this.tabBarElement.style.display = 'flex';
+        }, 4000);
+    }
+
+
+}
